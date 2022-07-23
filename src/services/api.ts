5ae20dc29a447e37410d8d66a6c54a350baf2b88
Yuri <yuriqpaiva@gmail.com>
2022-07-23 18:04:01 -0300

    Add node env api url

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
+const baseURL =
+  process.env.NODE_ENV === 'production'
+    ? 'https://upfi-rouge.vercel.app/'
+    : 'http://localhost:3000';
+
 export const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL,
 });
