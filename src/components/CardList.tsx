d89e2ff1b108f7501b45e63162829ce7e686225a
Yuri <yuriqpaiva@gmail.com>
2022-07-23 14:33:01 -0300

    Add image card view

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,32 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const { isOpen, onOpen, onClose } = useDisclosure();
 
   // TODO SELECTED IMAGE URL STATE
+  const [selectedImageUrl, setSelectedImageUrl] = useState<string>(null);
 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  const handleViewImage = (imgUrl: string): void => {
+    setSelectedImageUrl(imgUrl);
+    onOpen();
+  };
 
   return (
     <>
       {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing="10">
+        {cards.map(card => (
+          <Card data={card} viewImage={handleViewImage} />
+        ))}
+      </SimpleGrid>
 
       {/* TODO MODALVIEWIMAGE */}
+      <ModalViewImage
+        isOpen={isOpen}
+        onClose={onClose}
+        imgUrl={selectedImageUrl}
+      />
     </>
   );
 }
--- src/components/Modal/ViewImage.tsx
+++ src/components/Modal/ViewImage.tsx
@@ -20,4 +20,24 @@ export function ModalViewImage({
   imgUrl,
 }: ModalViewImageProps): JSX.Element {
   // TODO MODAL WITH IMAGE AND EXTERNAL LINK
+  return (
+    <Modal isOpen={isOpen} onClose={onClose} isCentered>
+      <ModalOverlay />
+      <ModalContent bg="pGray.800">
+        <ModalBody maxWidth={900} height={600} p={0}>
+          <Image src={imgUrl} width="100%" height="100%" />
+        </ModalBody>
+        <ModalFooter
+          display="flex"
+          justifyContent="start"
+          h="8"
+          borderBottomRadius={6}
+        >
+          <Link fontSize="0.875rem" href={imgUrl} isExternal>
+            Abrir original
+          </Link>
+        </ModalFooter>
+      </ModalContent>
+    </Modal>
+  );
 }
