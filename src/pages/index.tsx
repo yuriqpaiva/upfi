cb74227f57fca1e87aef71f918fd1c9a17476bc8
Yuri <yuriqpaiva@gmail.com>
2022-07-23 13:37:42 -0300

    Fetch home data

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -19,17 +19,39 @@ export default function Home(): JSX.Element {
   } = useInfiniteQuery(
     'images',
     // TODO AXIOS REQUEST WITH PARAM
-    ,
+    async ({ pageParam = null }) => {
+      const response = await api.get('/api/images', {
+        params: { after: pageParam },
+      });
+      return response;
+    },
+    {
+      getNextPageParam: lastPage => {
+        if (lastPage.data.after) {
+          return lastPage.data.after;
+        }
+        return null;
+      },
+    }
     // TODO GET AND RETURN NEXT PAGE PARAM
   );
 
   const formattedData = useMemo(() => {
     // TODO FORMAT AND FLAT DATA ARRAY
+    return data?.pages.map(page => page.data.data).flat();
   }, [data]);
 
   // TODO RENDER LOADING SCREEN
 
+  if (isLoading) {
+    return <Loading />;
+  }
+
   // TODO RENDER ERROR SCREEN
+
+  if (isError) {
+    return <Error />;
+  }
 
   return (
     <>
